f53e9c9d997fc83c33e191c0bcb56801c41c1202
lhmd <2683235973@qq.com>
2023-01-17 20:16:15 +0800

    完结撒花！

---
+++ 23_test.py
@@ -0,0 +1,41 @@
+import torch
+import torchvision
+from PIL import Image
+from torch import nn
+
+image_path = "myimage/1.png"
+image = Image.open(image_path)
+image = image.convert('RGB')
+
+transform = torchvision.transforms.Compose([torchvision.transforms.Resize((32, 32)),
+                                            torchvision.transforms.ToTensor()])
+image = transform(image)
+# print(image.shape)
+
+# 搭建神经网络
+class LHMD(nn.Module):
+    def __init__(self):
+        super(LHMD, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64*4*4, 64),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+model = torch.load("model/20_lhmd3.pth", map_location=torch.device('cpu'))
+# print(model)
+image = torch.reshape(image, (1, 3, 32, 32))
+model.eval()
+with torch.no_grad():
+    output = model(image)
+print(output.argmax(1))
